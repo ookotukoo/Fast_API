11c23d0aff063bb7703768be79a0a754004b4beb
Hocopor <redhotchilipeppersj@mail.ru>
2025-07-16 21:30:23 +0300

    https://fastapi.tiangolo.com/ru/tutorial/query-params-str-validations/#_7

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ app = FastAPI()
 
 
 @app.get("/items/")
-async def read_items(q: Annotated[list[str] | None, Query()] = None):
-    query_items = {"q": q}
-    return query_items
+async def read_items(q: Annotated[str | None, Query(alias="item-query")] = None):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
